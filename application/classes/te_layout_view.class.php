e03e2a79790a9df077254f30b052c1b08c9eaa51
Thomas van den Doel <tmvandendoel>
2020-12-11 21:45:22 +0100

    Allow $data to be passed as paramter to view constructor

--- application/classes/te_layout_view.class.php
+++ application/classes/te_layout_view.class.php
@@ -3,10 +3,11 @@
 //around it by default.
 class te_layout_view Extends te_view
 {
-  public function __construct($template)
+  public function __construct($template, $data = [])
   {
     $this->template = "layout";
-    $this->add_subview("page", new te_view($template));
+    $this->data = $data;
+    $this->add_subview("page", new te_view($template, $data));
 
     //add main.css by default:
     tank_engine::add_css("main");
