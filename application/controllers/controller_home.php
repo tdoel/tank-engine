c21a46e9ccd856154ca05a08bd126df7617ada05
Thomas van den Doel <tmvandendoel>
2020-12-11 21:46:13 +0100

    Add installation instructions to default home page

--- application/controllers/controller_home.php
+++ application/controllers/controller_home.php
@@ -4,6 +4,17 @@ class controller_home Extends te_controller
   public function index($args = null)
   {
     tank_engine::set_title("Home - index");
-    return new te_layout_view("home");
+
+    //some data gathering for the default home page
+    $data["url_root"] = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https://" : "http://"
+      . $_SERVER["HTTP_HOST"]
+      . $_SERVER["REQUEST_URI"];
+    $data["url_root"] = rtrim($data["url_root"],"/");
+    $data["config_origin"] = TE_DOCUMENT_ROOT . "/framework/boot/0.boot.php";
+    $data["config_destination"] = TE_DOCUMENT_ROOT . "/application/boot/0.boot.php";
+    $data["htaccess_path"] = TE_DOCUMENT_ROOT . "./htaccess";
+
+
+    return new te_layout_view("home",$data);
   }
 }
--- application/views/home.php
+++ application/views/home.php
@@ -1,4 +1,24 @@
 <h1>Tank Engine home</h1>
 <p>
-  Choo choo! Welcome to the default homepage of Tank Engine.
+  Choo choo! Welcome to the default homepage of Tank Engine. Below you can find
+  a few things you may need to do to complete the setup.
 </p>
+
+<h2>1. Setting up 0.config.php</h2>
+<ol>
+  <li>Copy <b><?= $this->data["config_origin"] ?></b> to <b><?= $this->data["config_destination"]; ?></b></li>
+  <li>Open <b><?= $this->data["config_destination"]; ?></b> with your favourite text-editor.</li>
+  <li>Change line 14 to <b>define("TE_URL_ROOT","<?= $this->data["url_root"]; ?>");</b></li>
+</ol>
+
+<h2>2. Setting up .htaccess</h2>
+To test if routing is functional, visit <?= $this->link("home/index","this link"); ?>. This should bring you to the page you are viewing right now. If you get a file-not-found error, follow these instructions:
+<ol>
+  <li>Open <b><?= $this->data["htaccess_path"]; ?></b> with your favourite text-editor.</li>
+  <li>Depening on your situation, either</li>
+  <ol>
+    <li>Uncomment line 6; or</li>
+    <li>Uncomment line 12 and alter it to your needs.</li>
+  </ol>
+  <li>Once you have saved the changes, you can verify .htaccess is configured correctly by visiting the link above.</li>
+</ol>
